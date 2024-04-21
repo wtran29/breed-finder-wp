1017eef28a061df240154fc9be34d417165df898
William Tran <wtran29@gmail.com>
2024-04-21 12:06:46 -0700

    setup video to be sent to worker pool and video queue channel

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	// define number of workers and jobs
 	const numJobs = 4
-	const numWorkers = 2
+	const numWorkers = 1
 
 	// create chan for work and results
 	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
@@ -26,8 +26,9 @@ func main() {
 	wp.Run()
 
 	// create 4 videos to send to worker pool
-
+	video := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
 	// send videos to worker pool
-
+	videoQueue <- streamer.VideoProcessingJob{Video: video}
 	// print out results
+
 }
