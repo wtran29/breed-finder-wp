4a3ffed8d4017c6efae26f52afec7b7a9c2827a0
William Tran <wtran29@gmail.com>
2024-04-22 09:57:58 -0700

    update encode to mp4 function for video encoding

--- streamer/encoders.go
+++ streamer/encoders.go
@@ -1,5 +1,11 @@
 package streamer
 
+import (
+	"fmt"
+
+	"github.com/xfrr/goffmpeg/transcoder"
+)
+
 // Encoder is an interface for encoding video. Any type that wants to satisfy
 // this interface must implement all its methods.
 type Encoder interface {
@@ -12,5 +18,27 @@ type VideoEncoder struct{}
 
 // EncodeToMP4 takes a Video object and base filename and encodes to MP4 format
 func (ve *VideoEncoder) EncodeToMP4(v *Video, baseFileName string) error {
+	// create a transcoder
+	trans := new(transcoder.Transcoder)
+
+	// build output path
+	outputPath := fmt.Sprintf("%s/%s", v.OutputDir, baseFileName)
+
+	// initialize the transcoder
+	err := trans.Initialize(v.InputFile, outputPath)
+	if err != nil {
+		return err
+	}
+
+	// set codec 
+	trans.MediaFile().SetVideoCodec("libx264")
+
+	// start transcoding process
+	done := trans.Run(false)
+
+	err = <-done
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- streamer/go.mod
+++ streamer/go.mod
@@ -1,3 +1,5 @@
 module github.com/wtran29/streamer
 
 go 1.22.1
+
+require github.com/xfrr/goffmpeg v1.0.0
